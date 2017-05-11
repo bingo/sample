80c36f5e424090432e2b9e0886ff3e207618d991
vagrant <vagrant@homestead>
2017-05-11 05:59:19 +0000

    Beautify layout and static pages

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="http://vvip.ai/">
+        私人助理，随时用心恭候您！
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="/about">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
